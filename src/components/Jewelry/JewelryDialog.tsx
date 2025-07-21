41da9153601ac20cd0a9e3a2efabd52621af0bc2
Agus-dot1 <agus1dev@gmail.com>
2025-07-21 20:42:33 -0300

    Update JewelryDialog.tsx

--- src/components/Jewelry/JewelryDialog.tsx
+++ src/components/Jewelry/JewelryDialog.tsx
@@ -30,6 +30,7 @@ const JewelryDialog: React.FC<JewelryDialogProps> = ({ item, onClose, onOpenCart
     });
 
     addToCart(productForCart);
+    // showSuccess('Producto Agregado', `${item.name} - Modelo ${selectedModel + 1} fue agregado al carrito`);
     onClose();
     onOpenCart();
   };
